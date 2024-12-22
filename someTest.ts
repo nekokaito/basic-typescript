3294e9b26789e3631de42dc3a949b239dece2f57
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-12-22 20:27:07 +0600

    someTest.ts

---
+++ someTest.ts
@@ -0,0 +1,13 @@
+// type Neko = {
+//      num1: number,
+//      num2: number,
+//      kasa: string
+// }
+
+const testData = (num1:number, num2:number, kasa:string)=> {
+
+     console.log(kasa);
+     return num1 * num2;
+}
+
+testData(3,2,"working");
