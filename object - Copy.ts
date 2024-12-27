bb802cc5421a7eb3a9067a1636b00fc6f95f9317
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-12-27 23:39:57 +0600

    uPDATED

---
+++ object - Copy.ts
@@ -0,0 +1,16 @@
+type Neko = { 
+     name: string,
+     age: number,
+     adult: boolean,
+     bday: string
+ }
+
+
+const Neko:Neko = {
+  name: "Kaito",
+  age: 24,
+  adult: true,
+  bday: "21/08/2000",
+};
+
+console.log(Neko);
